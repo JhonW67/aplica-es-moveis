d1cb864301a4fc0894b9c191ab123fa3891993ef
João Vitor <joaoocosta161@gmail.com>
2026-05-15 10:34:26 -0400

    inicialização da estrutura base

---
+++ IA2/DevCard/src/app/cadastro.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native/types_generated/index"
+
+export default function CadastroScreen(){
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Cadastro Screen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
---
+++ IA2/DevCard/src/app/preview.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import { SafeAreaView } from "react-native/types_generated/index"
+
+export default function PreviewScreen(){
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Preview Screen</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
